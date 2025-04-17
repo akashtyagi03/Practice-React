3380ca7dc23db39830767ed409b03063f4d5efd7
Aakash tyagi <akashtyagi780251@gmail.com>
2025-04-17 21:40:04 +0530

    add some features

--- Practice-Task-In-React/src/App.jsx
+++ Practice-Task-In-React/src/App.jsx
@@ -3,15 +3,12 @@ import Header from './component/Header';
 import Main from './component/Main';
 
 function App() {
-  const [showText, setShowText] = useState(false);
 
 
   return (
    <div>
     <Header/>
     <Main/>
-    <p>here something missing</p>
-    <p>here something missing</p>
    </div>
   );
 }
--- Practice-Task-In-React/src/component/Main.jsx
+++ Practice-Task-In-React/src/component/Main.jsx
@@ -1,18 +1,54 @@
 import React from 'react'
-import Timer from './Timer'
 
-const Main = () => {
+const Main = (setTime) => {
+    const [time, setTime] = useState(25*60);
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(()=>{
+        let interval = null;
+        if (isActive && time>0){
+            interval = setInterval(()=>{
+                setTime(prev => prev - 1)
+            }, 1000)
+        }else if (!isActive && time !== 0){
+            clearInterval(interval)
+        }
+        return ()=>clearInterval(interval)
+        }, [isActive, time])
+    
+
+    const Start = () => setIsActive(true)
+    const handlestop = () => setIsActive(false)
+    const handlerestart = () => {
+        setIsActive(false);
+        setTime(25*60);
+    }
+
+    function shorttime() {
+        setTime(5 * 60)
+    }
+    function longtime() {
+        setTime(5 * 60)
+    }
+
     return (
-        <div className="flex justify-center h-screen">
-            <div className="border-2 w-[30rem] h-[50rem]">
-                <div className='flex flex-wrap justify-around p-3'>
-                    <button className='border-1 rounded-sm p-1'>Pomodoro</button>
-                    <button className='border-1 rounded-sm p-1'>Short Break</button>
-                    <button className='border-1 rounded-sm p-1'>Long Break</button>
+        <>
+            <div className="flex justify-center">
+                <div className="h-15 w-100">
+                    <div className='flex flex-wrap justify-around p-2 '>
+                        <button className='border-1 rounded-sm p-2 cursor-pointer'>Pomodoro</button>
+                        <button className='border-1 rounded-sm p-2 cursor-pointer' onClick={shorttime}>Short Break</button>
+                        <button className='border-1 rounded-sm p-2 cursor-pointer' onClick={longtime}>Long Break</button>
+                    </div>
                 </div>
-                {/* <Timer/> */}
             </div>
-        </div>
+            <div>
+                <div>{Math.floor(time / 60)}:{('0' + (time % 60)).slice(-2)}</div>
+                <button onClick={Start}>Start</button>
+                <button onClick={handlestop}>Stop</button>
+                <button onClick={handlerestart}>Reset</button>
+            </div>
+        </>
     )
 }
 
